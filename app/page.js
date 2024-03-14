81cbe8a0964c4f0f4ade6b911966343a46901463
Serhii Demchenko <svdemchenko@gmail.com>
2024-03-14 22:52:08 +0200

    add rout pages

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
+import Meals from "./meals/page";
+
 export default function Home() {
-  return (
-    <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Time to get started!
-      </h1>
-    </main>
-  );
+	return (
+		<main>
+			<h1 style={{ color: "white", textAlign: "center" }}>
+				Time to get started!
+			</h1>
+			<p>
+				<Link href="/meals">Meals</Link>
+			</p>
+			<p>
+				<Link href="/meals/share">Share Meal</Link>
+			</p>
+			<p>
+				<Link href="/community">Community</Link>
+			</p>
+		</main>
+	);
 }
