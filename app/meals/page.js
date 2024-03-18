285a8cd9ab7321a6be0ed78e2258e65db266ef3b
Serhii Demchenko <svdemchenko@gmail.com>
2024-03-18 22:04:51 +0200

    add meals page

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,25 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
 export default function MealsPage() {
 	return (
-		<main>
-			<h1>Meals Page</h1>
-		</main>
+		<>
+			<header className={classes.header}>
+				<h1>
+					Delicious meals, created{" "}
+					<span className={classes.highlight}>by you</span>
+				</h1>
+				<p>
+					Choose you favorite recipe and cook it yourself. It is easy and fun!
+				</p>
+				<p className={classes.cta}>
+					<Link href="/meals/share">Share your Favorite Recipe</Link>
+				</p>
+			</header>
+			<main className={classes.main}>
+				<MealsGrid meals={[]} />
+			</main>
+		</>
 	);
 }
