8c93ca633360439da718e6504cb98cf2337aec66
Serhii Demchenko <svdemchenko@gmail.com>
2024-03-19 22:49:34 +0200

    fix imagepicker prop

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -50,6 +50,7 @@ export default function ImagePicker({ label, name }) {
 					name={name}
 					ref={imageInput}
 					onChange={handleImageChange}
+					required
 				/>
 				<button
 					onClick={handlePickClick}
