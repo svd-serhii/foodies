03bbe3f709b86df03f0d3a37e654a5901cd58a96
Serhii Demchenko <svdemchenko@gmail.com>
2024-03-22 00:19:46 +0200

    add db actions

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,4 @@
+import { shareMeal } from "@/lib/actions";
 import ImagePicker from "@/components/meals/image-picker";
 import classes from "./page.module.css";
 
@@ -11,7 +12,7 @@ export default function ShareMealPage() {
 				<p>Or any other meal you feel needs sharing!</p>
 			</header>
 			<main className={classes.main}>
-				<form className={classes.form}>
+				<form className={classes.form} action={shareMeal}>
 					<div className={classes.row}>
 						<p>
 							<label htmlFor="name">Your name</label>
@@ -39,7 +40,7 @@ export default function ShareMealPage() {
 							required
 						></textarea>
 					</p>
-					<ImagePicker />
+					<ImagePicker label="Your image" name="image" />
 					<p className={classes.actions}>
 						<button type="submit">Share Meal</button>
 					</p>
