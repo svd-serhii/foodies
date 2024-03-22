8a5a5df1a20a603336475cfb08536406204ecf8a
Serhii Demchenko <svdemchenko@gmail.com>
2024-03-22 23:10:55 +0200

    add validation

--- app/meals/[mealSlug]/page.js
+++ app/meals/[mealSlug]/page.js
@@ -21,7 +21,7 @@ export default function MealDetailPage({ params }) {
 				<div className={classes.headerText}>
 					<h1>{meal.title}</h1>
 					<p className={classes.creator}>
-						by<a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+						by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
 					</p>
 					<p className={classes.summary}>{meal.summary}</p>
 				</div>
